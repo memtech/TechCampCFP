055d9289a51d4e4c483af93a7114f47d53abec50
Joe Ferguson <joe@joeferguson.me>
2015-05-27 17:49:37 -0500

    Copy configs over if every provision just in case

--- scripts/after.sh
+++ scripts/after.sh
@@ -4,5 +4,7 @@
 # add any commands you wish to this file and they will
 # be run after the Homestead machine is provisioned.
 echo "Running after.sh"
-
+cp /vagrant/phinx.yml.dist /vagrant/phinx.yml
+cp /home/vagrant/cfp.dev/config/development.dist.yml /home/vagrant/cfp.dev/config/development.yml
+cd /vagrant
 vendor/bin/phinx migrate --environment=development
