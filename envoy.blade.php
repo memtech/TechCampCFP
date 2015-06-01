2fc078378f932eb597d3c44346fe24a9f504730b
Joe Ferguson <joe@joeferguson.me>
2015-06-01 17:42:44 -0500

    Don't worry about composer update on deploy. Run manually

--- envoy.blade.php
+++ envoy.blade.php
@@ -5,6 +5,6 @@ cd ~/TechCampCFP
 git pull origin master
 cp ~/configs/production.yml ~/TechCampCFP/config/production.yml
 cp ~/configs/phinx.yml ~/TechCampCFP/phinx.yml
-composer update
+#composer update
 ~/TechCampCFP/vendor/bin/phinx migrate --environment=production
 @endtask
